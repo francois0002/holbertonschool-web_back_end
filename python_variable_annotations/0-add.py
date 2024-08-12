ce014829d7011a68d8663411d08b70090da22aee
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-12 11:20:44 +0200

    Add 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module return a sum of a float"""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two float numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b
