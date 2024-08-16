c138d8a0b217cbccbb5f7b088e7a6d11f106061e
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-16 11:59:22 +0200

    Correction

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 """
 Take the code from wait_n and alter it into a new function task_wait_n.
-The code is nearly identicalto wait_n except task_wait_random is being called.
+The code is nearly identical to wait_n except task_wait_random is being called.
 """
 import asyncio
-from random import uniform
 from typing import List
 
-def task_wait_n(n: int, max_delay: int) -> asyncio.Task:
-    """
-    Take the code from wait_n and alter it into a new function task_wait_n.
-    The code is nearly identicalto wait_n except task_wait_random is being called.
-    """
-    async def _wait_random():
 
-        delay = uniform(0, max_delay)
-        await asyncio.sleep(delay)
-        return delay
+task_wait_random = __import__('3-tasks').task_wait_random
 
-    async def run_tasks():
-        tasks = [asyncio.create_task(_wait_random()) for _ in range(n)]
-        return await asyncio.gather(*tasks)
 
-    return asyncio.create_task(run_tasks())
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ wait_n should return the list of all the delays (float values).
+    The list of the delays should be in ascending order without using sort
+    because of concurrency. """
+    delays: List = []
+
+    for i in range(n):
+        delays.append(task_wait_random(max_delay))
+
+    new_list: List[float] = []
+
+    for x in asyncio.as_completed(delays):
+        # wait for as_completed to return and put that in a variable
+        done: float = await x
+        new_list.append(done)
+
+    return new_list
