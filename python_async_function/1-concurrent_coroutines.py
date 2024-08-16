438fe9dbd0c645555b4557da2d09f85ee916c741
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-16 12:00:59 +0200

    change task 2

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,21 +5,29 @@ and write an async routine called wait_n that takes in 2 int arguments
 (in this order): n and max_delay. You will spawn wait_random n times
 with the specified max_delay.
 """
+from typing import List
 
-import asyncio
-import heapq
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> list:
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ wait_n should return the list of all the delays (float values).
     The list of the delays should be in ascending order without using sort()
     because of concurrency. """
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
+    delays: List[float] = []
 
-    sorted_delays = []
-    for delay in delays:
-        heapq.heappush(sorted_delays, delay)
+    for i in range(n):
+        delays.append(await wait_random(max_delay))
 
-    return [heapq.heappop(sorted_delays) for _ in range(len(sorted_delays))]
+    new_list: List[float] = []
+
+    while delays:
+        min = delays[0]  # arbitrary number in list
+        for x in delays:
+            if x < min:
+                min = x
+        new_list.append(min)
+        delays.remove(min)
+
+    return new_list
