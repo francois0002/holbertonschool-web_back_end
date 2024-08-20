3f5312b3a7497e9f20fcc19970bae927fa0478b8
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-20 15:33:04 +0200

    task 3

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python3
-"""
-From the previous file, import wait_n into 2-measure_runtime.py.
-
-Create a measure_time function with integers n and max_delay as arguments
-that measures the total execution time for wait_n(n, max_delay), and returns
-total_time / n. Your function should return a float.
-
-Use the time module to measure an approximate elapsed time.
-"""
-import time
+""" Measure times """
 import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+async def measure_runtime() -> float:
+    """
+        measure time and execute in paralallel
 
+        Args:
+            void
 
-def measure_time(n: int, max_delay: int) -> float:
-    """Measure the runtime of wait_n(n, max_delay)"""
-    start_time = time.time()
-    asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    return (end_time - start_time) / n
+        Return:
+            float random numbers
+    """
+    first_time = time.perf_counter()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    elapsed = time.perf_counter()
+
+    return (elapsed - first_time)
