3917a67e3a1db64681e378b2f4863bf40b22f9eb
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-21 13:00:29 +0200

    pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+""" The function should return a tuple of size two containing
+a start index and an end index corresponding to the range of
+indexes to return in a list for those particular pagination parameters.
+"""
 
 def index_range(page, page_size):
     """Return a tuple of size two containing a start index and an end index"""
