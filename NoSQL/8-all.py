3d54e2ee1120ab37ddf8b470bf6296255ec0cf47
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-23 09:30:35 +0200

    change insertOne by insert

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,3 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-print("hello le monde")
+""" that module lists all documents in a collection """
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection"""
+    return ()
