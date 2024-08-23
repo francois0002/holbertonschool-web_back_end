3b3f900208d0888dd29d226a18f7c50f999678a8
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-23 09:49:56 +0200

    task 8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+import pymongo
+
 """ that module lists all documents in a collection """
 
 def list_all(mongo_collection):
     """Lists all documents in a collection"""
-    return ()
+
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
+
