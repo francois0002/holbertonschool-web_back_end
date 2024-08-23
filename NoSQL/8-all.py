511c3b38efc16404db2e4607e1dd7cc277aaad7d
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-23 09:56:12 +0200

    change pycodestyle

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
+""" list all pymongo
+"""
 
-import pymongo
-
-""" that module lists all documents in a collection """
 
 def list_all(mongo_collection):
-    """Lists all documents in a collection"""
-
+    """ lists all documents in a collection
+    """
     documents = mongo_collection.find()
     return documents
-
